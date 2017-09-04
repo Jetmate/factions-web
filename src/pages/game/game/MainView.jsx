6f582ca34ce66645386d468c951115d7e69af0ef
Jetmate <antonoutkine@gmail.com>
2017-09-04 12:42:51 -0500

    Add support for error messages

--- src/pages/game/game/MainView.jsx
+++ src/pages/game/game/MainView.jsx
@@ -25,6 +25,10 @@ class Component extends React.Component {
     if (canvas) {
       canvas.width = window.innerWidth
       canvas.height = window.innerHeight
+      window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+      })
       let ctx = canvas.getContext('2d')
       init(ctx, this.props.socket, this.props.dispatch)
     }
@@ -38,10 +42,6 @@ function init (ctx, socket, reduxDispatch) {
   document.body.oncontextmenu = (e) => {
     e.preventDefault()
   }
-  window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-  })
 
   const spriteSheetImage = new Image()
   spriteSheetImage.src = 'media/spritesheet.png'
--- src/pages/setup/index.jsx
+++ src/pages/setup/index.jsx
@@ -5,14 +5,23 @@ import Center from '../../component/Center.jsx'
 class Component extends React.Component {
   render () {
     return (
-      <Center>
-        <h1>Factions.io</h1>
-        <h3>open-world, survival, shooter...all in an io game!</h3>
-        <form action="/signup" method="post">
-          <input name="id" />
-          <button>go!</button>
-        </form>
-      </Center>
+        <div className="vertical-center">
+          <div className="horizontal-center">
+            <div className="element">
+              <h1>Factions.io</h1>
+              <h3>open-world, survival, shooter...all in an io game!</h3>
+              <form action="/signup" method="post">
+                <input name="id" />
+                <button>go!</button>
+              </form>
+            </div>
+            {window.error ? (
+              <div className="error">
+                <p>{window.error}</p>
+              </div>
+            ) : ''}
+          </div>
+        </div>
     )
   }
 }
