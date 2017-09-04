3cc73988c233d1837ad14b20bf501eb27353ee37
Jetmate <antonoutkine@gmail.com>
2017-09-04 10:56:15 -0500

    Remove debug text

--- src/pages/game/game/component/Leaderboard.jsx
+++ src/pages/game/game/component/Leaderboard.jsx
@@ -6,15 +6,6 @@ export default class Component extends React.Component {
 
     this.state = JSON.parse(window.leaderboard)
     this.state[window.id] = 0
-    this.state['a'] = 0
-    this.state['b'] = 0
-    this.state['c'] = 0
-    this.state['d'] = 0
-    this.state['e'] = 0
-    this.state['w'] = 0
-    this.state['h'] = 0
-    this.state['k'] = 0
-    this.state['g'] = 0
 
     this.props.socket.on('playerDeath', (playerId, killerId) => {
       delete this.state[playerId]
